951a6a2c1d692e289de3e33d05db38d8714b7a85
giantthinker <ramadanabdel24@gmail.com>
2020-05-07 05:34:11 +0100

    Initial commit

---
+++ numberguess/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3.8
+
+import random
+
+print('Welcome to the Guess game. You can quit any time by enterying q or ctrl + c')
+
+def init():  
+    try:
+        number = int(input('enter a number between 1 and 10: '))
+        return number
+    except NameError:
+        if(EOFError):
+            print('That input is empty')
+        print('Sorry, that is not a valid input')
+
+while True:
+
+    number = init()
+
+    randonnumber = random.randint(1, 10)
+
+    if (number == randonnumber) :
+        print('Hurray! Your guess is correct. ' + str(randonnumber) + ' is the correct guess')
+
