0265c45668212a5da3ee8ac0b53790cd66990b92
giantthinker <ramadanabdel24@gmail.com>
2020-05-07 13:15:16 +0100

    updated

--- numberguess/main.py
+++ numberguess/main.py
@@ -13,6 +13,8 @@ def init():
             print('That input is empty')
         print('Sorry, that is not a valid input')
 
+score = 0
+
 while True:
 
     number = init()
@@ -20,5 +22,16 @@ while True:
     randonnumber = random.randint(1, 10)
 
     if (number == randonnumber) :
+        score = score + 1
         print('Hurray! Your guess is correct. ' + str(randonnumber) + ' is the correct guess')
+    
+        cont = input('Play again? "y" or "n" :')
+
+        try:
+            if(cont != 'y' or cont != 'n'):
+                print('Enter y or n')
+            if(cont == 'n'):
+                break
+        except NameError:
+            print('Wrong input')
 
